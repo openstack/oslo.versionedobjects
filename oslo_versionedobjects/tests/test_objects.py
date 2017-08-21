8e5deeb238260856013aec1d9d34ea48df7e99cc
Chris Dent <cdent@anticdent.org>
2017-08-21 16:53:56 +0100

    Add a TimestampedObject mixin
    Copy the NovaTimestampObject, added in
    I556d93741f1ee965c960c6099fbf2c57f31c6744, to OVO. This provides
    a simple way for an object that is backed by a database table with
    updated_at and created_at fields to include those fields in the
    object.
    
    This functionality is likely useful beyond the scope of nova and
    will be needed when the objects used by placement service are
    extracted from nova.
    
    The tests may be a bit overkill: they mostly confirm that inheriting
    fields works as expected and the DateTimeField behaves as a
    DateTimeField.
    
    Change-Id: Icb08cc7ff5707a0bfe77422b4082ef30e5093a59

--- oslo_versionedobjects/tests/test_objects.py
+++ oslo_versionedobjects/tests/test_objects.py
@@ -23,6 +23,7 @@ import mock
 from oslo_context import context
 from oslo_serialization import jsonutils
 from oslo_utils import timeutils
+import testtools
 from testtools import matchers
 
 from oslo_versionedobjects import base
@@ -2441,3 +2442,42 @@ class TestListObjectConcat(test.TestCase):
             return [] + obj
 
         self.assertRaises(TypeError, add, list1)
+
+
+class TestTimestampedObject(test.TestCase):
+    """Test TimestampedObject mixin.
+
+    Do this by creating an object that uses the mixin and confirm that the
+    added fields are there and in fact behaves as the DateTimeFields we desire.
+    """
+
+    def setUp(self):
+        super(TestTimestampedObject, self).setUp()
+
+        @base.VersionedObjectRegistry.register_if(False)
+        class MyTimestampedObject(base.VersionedObject,
+                                  base.TimestampedObject):
+            fields = {
+                'field1': fields.Field(fields.String()),
+            }
+
+        self.myclass = MyTimestampedObject
+        self.my_object = self.myclass(field1='field1')
+
+    def test_timestamped_has_fields(self):
+        self.assertEqual('field1', self.my_object.field1)
+        self.assertIn('updated_at', self.my_object.fields)
+        self.assertIn('created_at', self.my_object.fields)
+
+    def test_timestamped_holds_timestamps(self):
+        now = timeutils.utcnow(with_timezone=True)
+        self.my_object.updated_at = now
+        self.my_object.created_at = now
+        self.assertEqual(now, self.my_object.updated_at)
+        self.assertEqual(now, self.my_object.created_at)
+
+    def test_timestamped_rejects_not_timestamps(self):
+        with testtools.ExpectedException(ValueError, '.*parse date.*'):
+            self.my_object.updated_at = 'a string'
+        with testtools.ExpectedException(ValueError, '.*parse date.*'):
+            self.my_object.created_at = 'a string'
